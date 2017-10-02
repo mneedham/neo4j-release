4c4df613c271ea4f5b19a9a39cd31c52b8a6413a
Mark Needham <mark.needham@neotechnology.com>
2017-10-02 16:37:04 +0100

    single instance Neo4j

--- jobs/neo4j/templates/ctl.bash
+++ jobs/neo4j/templates/ctl.bash
@@ -2,14 +2,16 @@
 
 RUN_DIR=/var/vcap/sys/run/neo4j
 
-export JAVA_HOME=/var/vcap/packages/java
+export JAVA_HOME=/var/vcap/packages/openjdk_1.8.0/jre
 export NEO4J_HOME=/var/vcap/packages/neo4j
 export NEO4J_PIDFILE=${RUN_DIR}/pid
 export NEO4J_LOGS=/var/vcap/sys/log/neo4j
 export NEO4J_CONF=/var/vcap/jobs/neo4j/conf
-export NEO4J_DATA=/var/vcap/store/neo4j
 
+mkdir -p $RUN_DIR
 mkdir -p $NEO4J_LOGS
 mkdir -p $NEO4J_CONF
+chown -R vcap:vcap $NEO4J_LOGS $RUN_DIR /var/vcap/store/neo4j/data
 
-/var/vcap/packages/neo4j/bin/neo4j $1
+echo $$ > ${RUN_DIR}/pid
+exec /var/vcap/packages/neo4j/bin/neo4j $1 >> $NEO4J_LOGS/$1_stdout.log 2>> $NEO4J_LOGS/$1_stderr.log
